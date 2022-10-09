a89899ff3d2f736dcd4a50d59a94b65f7ab6162a
tahsincanpolat <tahsincanpolat@gmail.com>
2022-10-09 12:53:01 +0300

    homepage - product page

---
+++ ETrade/ETrade/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using ETrade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETrade.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        Context db;
+        public ProductController()
+        {
+            db = new Context();
+        }
+        public ActionResult Product(int id)
+        {
+            return View(db.Products.Where(x=>x.SubCategoryID == id).ToList());
+        }
+    }
+}

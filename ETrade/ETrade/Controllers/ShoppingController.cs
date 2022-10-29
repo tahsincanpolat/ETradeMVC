f292d1a9c825de834db098479fa498abfc4b045e
tahsincanpolat <tahsincanpolat@gmail.com>
2022-10-29 11:55:12 +0300

    complete shopping

--- ETrade/ETrade/Controllers/ShoppingController.cs
+++ ETrade/ETrade/Controllers/ShoppingController.cs
@@ -119,6 +119,83 @@ namespace ETrade.Controllers
             return View(db.Customers.Find(TemporaryUserData.UserID));
         }
 
+
+        public ActionResult CompleteShopping(FormCollection frm)
+        {
+            Payment payment = new Payment()
+            {
+                Type = int.Parse(frm["paymentType"]),
+                Balance = 5000,
+                CreditAmount = 5000,
+                DebitAmount = 5000,
+                PaymentDateTime = DateTime.Now
+            };
+
+            db.Payments.Add(payment);
+
+            if (frm["update"] == "on")
+            {
+                Customer customer = db.Customers.Find(TemporaryUserData.UserID);
+                customer.FirstName = frm["FirstName"];
+                customer.LastName = frm["LastName"];
+                customer.UserName = frm["UserName"];
+                customer.Email = frm["Email"];
+                customer.Address = frm["Address"];
+                customer.City = frm["City"];
+            }
+
+            ShippingDetail shipping = new ShippingDetail()
+            {
+               FirstName = frm["FirstName"],
+               LastName = frm["LastName"],
+               Email = frm["Email"],
+               Address = frm["Address"],
+               City = frm["City"]
+            };
+
+            db.ShippingDetails.Add(shipping);
+
+            db.SaveChanges();
+
+            List<OrderDetail> cart = db.OrderDetails.Where(x => x.IsCompleted == false && x.CustomerID == TemporaryUserData.UserID).ToList();
+
+            foreach (var item in cart)
+            {
+                item.IsCompleted = true;
+                item.Product.UnitInStock -= item.Quantity;  // Stoktan eksiltme işlemi
+
+                Order order = new Order()
+                {
+                    PaymentID = payment.PaymentID,
+                    ShippingID = shipping.ShippingID,
+                    OrderDetailID = item.OrderDetailID,
+                    Discount = item.Discount,
+                    TotalAmount = item.TotalAmount,
+                    IsCompleted = true,
+                    OrderDate = DateTime.Now,
+                    Dispatched = false,
+                    DispatchDate = DateTime.Now.AddDays(3),
+                    Shipped = false,
+                    ShippedDate = DateTime.Now.AddDays(4),
+                    Deliver = false,
+                    DeliveryDate = DateTime.Now.AddDays(5),
+                    CancelOrder = false
+                };
+
+                db.Orders.Add(order);
+            }
+            db.SaveChanges();
+
+            return RedirectToAction("FinishShopping","Shopping");
+
+        }
+
+        public ActionResult FinishShopping()
+        {
+            return View();
+        }
+
+
         #region Methods
 
         public void ControlCart(int id, int miktar = 1)
